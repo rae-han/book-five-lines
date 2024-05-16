4fb5a57152a65c5aff4e14423a13065faab36efe
raehan <raehan900809@gmail.com>
2024-05-16 09:09:25 +0900

    feat: 떨어진 돌 없어지는 버그 수정

--- index.ts
+++ index.ts
@@ -730,7 +730,7 @@ const handleInputs = () => {
 const updateTile = (x: number, y: number) => {
   if ((map[y][x].isStone() || map[y][x].isFallingStone())
     && map[y + 1][x].isAir()) {
-    map[y + 1][x].isFallingStone();
+    map[y + 1][x] = new FallingStone();
     map[y][x] = new Air();
   } else if ((map[y][x].isBox() || map[y][x].isFallingBox())
     && map[y + 1][x].isAir()) {
